6cf626768d0513b1b4ffbe4aa172e9fd9374e50d
PhiHungTF <phihungtf@gmail.com>
2022-12-07 11:09:44 +0700

    edit createFile.c

--- nachos-3.4/code/test/createFile.c
+++ nachos-3.4/code/test/createFile.c
@@ -2,14 +2,15 @@
 
 int main() {
 	OpenFileId id;
-	char buffer[11];
+	char buffer[12];
 	Create("test.txt");
 	id = Open("test.txt", 0);
-	Write("Hello World", 11, id);
+	Write("Hello, World", 12, id);
 	Close(id);
 	id = Open("test.txt", 1);
-	Read(buffer, 11, id);
+	Read(buffer, 12, id);
 	Close(id);
 	PrintString(buffer);
+	PrintString("\n");
 	return 0;
 }
