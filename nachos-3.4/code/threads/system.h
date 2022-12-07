1af43fd563fda92bc2ca987e8d71f18b10bbc97b
PhiHungTF <phihungtf@gmail.com>
2022-12-07 15:33:54 +0700

    add synch.h to system.h

--- nachos-3.4/code/threads/system.h
+++ nachos-3.4/code/threads/system.h
@@ -34,6 +34,7 @@ extern Timer *timer;                 // the hardware alarm clock
 
 #ifdef USER_PROGRAM
 #include "machine.h"
+#include "synch.h"
 #include "synchcons.h"
 extern Machine *machine;  // user program memory and registers
 extern SynchConsole *synchconsole;
