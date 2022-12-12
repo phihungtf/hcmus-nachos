5240fc25cae5e4143862cabec5fc78fc205a2633
PhiHungTF <phihungtf@gmail.com>
2022-12-12 14:27:23 +0700

    edit test

--- nachos-3.4/code/test/sort.c
+++ nachos-3.4/code/test/sort.c
@@ -1,32 +1,33 @@
-/* sort.c 
- *    Test program to sort a large number of integers.
- *
- *    Intention is to stress virtual memory system.
- *
- *    Ideally, we could read the unsorted array off of the file system,
- *	and store the result back to the file system!
- */
-
 #include "syscall.h"
+#define MAX_SIZE 50
 
-int A[1024];	/* size of physical memory; with code, we'll run out of space!*/
+int main() {
+    int size = 0, i, arr[MAX_SIZE + 1], j, temp;
+    do {
+        PrintString("Enter the number size of array [0,50] n = ");
+        size = ReadInt();
+    } while (size < 0 || size > 50);
 
-int
-main()
-{
-    int i, j, tmp;
+    for (i = 0; i < size; i++) {
+        PrintString("Enter arr[");
+        PrintChar(i + '0');
+        PrintString("] = ");
+        arr[i] = ReadInt();
+    }
 
-    /* first initialize the array, in reverse sorted order */
-    for (i = 0; i < 1024; i++)		
-        A[i] = 1024 - i;
+    for (i = 0; i < size - 1; i++) {
+        for (j = i + 1; j < size; j++) {
+            if (arr[i] > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
 
-    /* then sort! */
-    for (i = 0; i < 1023; i++)
-        for (j = i; j < (1023 - i); j++)
-	   if (A[j] > A[j + 1]) {	/* out of order -> need to swap ! */
-	      tmp = A[j];
-	      A[j] = A[j + 1];
-	      A[j + 1] = tmp;
-    	   }
-    Exit(A[0]);		/* and then we're done -- should be 0! */
+    for (i = 0; i < size; i++) {
+        PrintInt(arr[i]);
+        PrintChar(' ');
+    }
+    Halt();
 }
