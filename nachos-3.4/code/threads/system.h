d3215f15164b760ac2cc828ebccfe36070470e68
PhiHungTF <phihungtf@gmail.com>
2022-12-12 15:35:13 +0700

    Revert "rm some comments"
    This reverts commit e1ea2a54eb28c60ad357ab83de832896267642aa.

--- nachos-3.4/code/threads/system.h
+++ nachos-3.4/code/threads/system.h
@@ -1,9 +1,16 @@
-// system.h
+// system.h 
 //	All global variables used in Nachos are defined here.
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation
+// All rights reserved.  See copyright.h for copyright notice and limitation 
 // of liability and disclaimer of warranty provisions.
+
+/////////////////////////////////////////////////
+// 	DH KHTN - DHQG TPHCM			/
+// 	1512034 Nguyen Dang Binh		/
+// 	1512042 Nguyen Thanh Chung		/
+// 	1512123 Hoang Ngoc Duc			/
+/////////////////////////////////////////////////
 
 #ifndef SYSTEM_H
 #define SYSTEM_H
@@ -20,44 +27,44 @@
 #include "stable.h"
 
 // Initialization and cleanup routines
-extern void Initialize(int argc, char **argv);  // Initialization,
-                                                // called before anything else
-extern void Cleanup();                          // Cleanup, called when
-                                                // Nachos is done.
+extern void Initialize(int argc, char **argv); 	// Initialization,
+						// called before anything else
+extern void Cleanup();				// Cleanup, called when
+						// Nachos is done.
 
-extern Thread *currentThread;        // the thread holding the CPU
-extern Thread *threadToBeDestroyed;  // the thread that just finished
-extern Scheduler *scheduler;         // the ready list
-extern Interrupt *interrupt;         // interrupt status
-extern Statistics *stats;            // performance metrics
-extern Timer *timer;                 // the hardware alarm clock
+extern Thread *currentThread;			// the thread holding the CPU
+extern Thread *threadToBeDestroyed;  		// the thread that just finished
+extern Scheduler *scheduler;			// the ready list
+extern Interrupt *interrupt;			// interrupt status
+extern Statistics *stats;			// performance metrics
+extern Timer *timer;				// the hardware alarm clock
 
 #ifdef USER_PROGRAM
 #include "machine.h"
-#include "synch.h"
 #include "synchcons.h"
-extern Machine *machine;  // user program memory and registers
-extern SynchConsole *synchconsole;
+#include "synch.h"
+extern Machine* machine;	// user program memory and registers
+extern SynchConsole* synchconsole;
 
-extern Semaphore *addrLock;      // semaphore
-extern BitMap *gPhysPageBitMap;  // quan ly cac frame
-extern PTable *pTab;             // quan ly bang tien trinh
-extern STable *semTab;           // quan ly semaphore
+extern Semaphore *addrLock;	// semaphore
+extern BitMap *gPhysPageBitMap;	// quan ly cac frame
+extern PTable *pTab;		// quan ly bang tien trinh
+extern STable *semTab;		// quan ly semaphore
 #endif
 
-#ifdef FILESYS_NEEDED  // FILESYS or FILESYS_STUB
+#ifdef FILESYS_NEEDED 		// FILESYS or FILESYS_STUB 
 #include "filesys.h"
-extern FileSystem *fileSystem;
+extern FileSystem  *fileSystem;
 #endif
 
 #ifdef FILESYS
 #include "synchdisk.h"
-extern SynchDisk *synchDisk;
+extern SynchDisk   *synchDisk;
 #endif
 
 #ifdef NETWORK
 #include "post.h"
-extern PostOffice *postOffice;
+extern PostOffice* postOffice;
 #endif
 
-#endif  // SYSTEM_H
+#endif // SYSTEM_H
